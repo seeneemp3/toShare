2a093def9f97245294725ce03bd34b1ec236480d
seeneemp3 <seeneemp3@gmail.com>
2023-09-20 17:31:56 +0400

    cleanup

--- src/main/java/org/personal/WebConfig.java
+++
@@ -1,9 +0,0 @@
-package org.personal;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-@EnableWebMvc
-@Configuration
-public class WebConfig {
-}
